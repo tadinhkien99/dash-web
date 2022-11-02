fd8f58c65a30a602795e65281244de6b45633419
KuroKien <66372098+tadinhkien99@users.noreply.github.com>
2022-11-02 15:43:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def page_return(pathname):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host='127.0.0.1', port='8010')
+    app.run_server(debug=True, host='0.0.0.0', port='8010')
